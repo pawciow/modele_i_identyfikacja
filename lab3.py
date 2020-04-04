754f40430dd61305c3e1e59ee690e778607bdc25
pawciow <pawciow123@gmail.com>
2020-04-04 22:01:33 +0200

    Finished lab 3 tasks

--- lab3.py
+++ lab3.py
@@ -1,13 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
+# This code sucks but i'm too tired to refractor. I don't think anyone will see it
 
 class Lab3:
-    def __init__(self, length, y_normal):
+    def __init__(self, length, y_normal, L):
+        self.L = L
         self.length = length
         self.first_est = np.zeros(length)
         self.second_est = np.zeros(length)
         self.third_est = np.zeros(length)
+        self.num = int(length/L)
+        self.first_est_err = np.zeros(self.num)
+        self.second_est_err = np.zeros(self.num)
+        self.third_est_err = np.zeros(self.num)
         self.y = y_normal
 
     def first_esteem(self, n): #Estymator wartości oczekiwanej
@@ -44,47 +50,106 @@ class Lab3:
         for i in range(self.length):
             self.third_est[i] = self.third_esteem(i + 1)
 
+    def calculate_first_esteem_error(self):
+        mean = np.mean(self.y)
+        tmp_sum = 0
+        number_of_calculations = self.num
+        for i in range(number_of_calculations):
+            for j in range(self.L):
+                tmp_sum += pow(self.first_est[self.L * i + j] - mean, 2)
+            self.first_est_err[i] = tmp_sum/self.L
+            tmp_sum = 0
 
-def first(n, y):
-    a = Lab3(n, y)
+    def calculate_second_esteem_error(self):
+        mean = np.var(self.y)
+        tmp_sum = 0
+        number_of_calculations = self.num
+        for i in range(number_of_calculations):
+            for j in range(self.L):
+                tmp_sum += pow(self.second_est[self.L * i + j] - mean, 2)
+            self.second_est[i] = tmp_sum/self.L
+            tmp_sum = 0
+
+    def calculate_third_esteem_error(self):
+        mean = np.var(self.y)
+        tmp_sum = 0
+        number_of_calculations = self.num
+        for i in range(number_of_calculations):
+            for j in range(self.L):
+                tmp_sum += pow(self.third_est[self.L * i + j] - mean, 2)
+            self.third_est_err[i] = tmp_sum/self.L
+            tmp_sum = 0
+
+
+
+    def subplot__all_for_first_est(self):
+        fig, (ax1, ax2) = plot.subplots(2, 1)
+        ax1.plot(self.first_est)
+        ax2.plot(self.first_est_err)
+        ax1.set_title('Estymacja')
+        ax2.set_title('Błąd empiryczny')
+        plot.show()
+
+    def subplot__all_for_second_est(self):
+        fig, (ax1, ax2) = plot.subplots(2, 1)
+
+        ax1.plot(self.second_est)
+        ax2.plot(self.second_est_err)
+        ax1.set_title('Estymacja')
+        ax2.set_title('Błąd empiryczny')
+        plot.show()
+
+    def subplot__all_for_third_est(self):
+        fig, (ax1, ax2) = plot.subplots(2, 1)
+
+        ax1.plot(self.third_est)
+        ax2.plot(self.third_est_err)
+        ax1.set_title('Estymacja')
+        ax2.set_title('Błąd empiryczny')
+        plot.show()
+
+
+
+def first(n, y, L):
+    a = Lab3(n, y, L)
     a.calculate_first_esteem()
-    plot.title = "Pierwszy estymator"
-    plot.plot(a.first_est)
-    plot.show()
+    a.calculate_first_esteem_error()
+    a.subplot__all_for_first_est()
 
 
-def second(n, y):
-    a = Lab3(n, y)
+def second(n, y, L):
+    a = Lab3(n, y, L)
     a.calculate_second_esteem()
-    plot.title = "Drugi estymator"
-    plot.plot(a.second_est)
-    plot.show()
+    a.calculate_second_esteem_error()
+    a.subplot__all_for_second_est()
 
-
-def third(n, y):
-    a = Lab3(n, y)
+def third(n, y, L):
+    a = Lab3(n, y, L)
     a.calculate_third_esteem()
-    plot.title = "Trzeci estymator"
-    plot.plot(a.third_est)
-    plot.show()
+    a.calculate_third_esteem_error()
+    a.subplot__all_for_third_est()
 
 
-def demo(n, y_normal):
+def demo(n, y_normal, L):
     plot.plot(y_normal)
     # plot.show()
     # plot.hist(y_normal, bins='auto')
     plot.show()
-    first(n, y_normal)
-    second(n, y_normal)
-    third(n, y_normal)
+    first(n, y_normal, L)
+    second(n, y_normal, L)
+    third(n, y_normal, L)
+    print("END OF CALCULATIONS")
 
 
-mu, sigma = 0, 0.1  # mean and standard deviation
-length = 10000
-# demo(len, np.random.normal(mu, sigma, len))
-y = np.random.standard_cauchy(length)
-y= y[(y>-25) & (y<25)]  # truncate distribution so it plots well
+# mu, sigma = 0, 0.1  # mean and standard deviation
+mu, sigma = 1, 0.4  # mean and standard deviation
+L = 10
+length = 1000
+demo(length, np.random.normal(mu, sigma, length), L)
+# y = np.random.standard_cauchy(length)
+# y= y[(y>-25) & (y<25)]  # truncate distribution so it plots well
 
-demo(len(y), y)
-print(np.var(y))
-print(np.mean(y))
+# demo(len(y), y)
+# print(np.var(y))
+# print(np.mean(y))
+
