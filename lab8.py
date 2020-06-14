a435f2c8f27eba1652b2821bbd372b88bb36aabc
pawciow <pawciow123@gmail.com>
2020-06-14 14:31:34 +0200

    End of report 3

--- lab8.py
+++ lab8.py
@@ -4,46 +4,57 @@ import matplotlib.pyplot as plt
 
 # X = np.array([[1, 2, 3], [4, 5, 6]])
 
-
+#Project is not pretty, I had to do it quickly
 def white_noise(samples):
     mean = 0
-    std = 1
+    std = 10
 
     return np.random.normal(mean, std, size=samples)
 
 
 class Model:
     def __init__(self, number_of_inputs, repeats, max_int):
+        self.numb = number_of_inputs
         self.X = np.random.normal(max_int, size=(number_of_inputs, repeats))
         self.Z = white_noise(number_of_inputs)
-        self.a = np.random.randint(max_int, size=repeats)
-        self.Y = self.X.dot(self.a) + self.Z
+        # self.a = np.random.randint(max_int, size=repeats)
+        self.a = np.array([4,2,2,2])
+        self.Y = self.X.dot(self.a) #+ self.Z
 
-    def estimate_least_squares(self):
-        self.a_approx = np.linalg.inv((self.X.T @ self.X)) @ self.X.T @ self.Y
-        self.Y_approx = self.X.dot(self.a_approx)
+        self.L = 1000
+
+    def calculate_error(self):
+        error = 0
+        for i in range(self.L):
+            Y_L = self.Y + white_noise(self.numb)
+            a_approx = np.linalg.inv((self.X.T @ self.X)) @ self.X.T @ Y_L
+            error += (abs(a_approx - self.a))**2
+
+        error = error/self.L
+        print(error)
+
 
     def plot_all(self):
         plt.plot(self.Y, label='Prawdziwe wyjście')
         plt.plot(self.Y_approx, label='Wyjście aproksymowane')
         plt.legend()
         plt.show()
+    #
+    # def plot_white_noise(self):
+    #     plt.plot(self.Z, label='Zakłócenia')
+    #
+    # def plot_imagesc(self):
+    #     self.covariance_matrix = np.cov(np.linalg.inv((self.X.T @ self.X)))
+    #     plt.imshow(self.covariance_matrix, extent=[0, 1, 0, 1])
+    #     plt.show()
 
-    def plot_white_noise(self):
-        plt.plot(self.Z, label='Zakłócenia')
 
-    def plot_imagesc(self):
-        self.covariance_matrix = np.cov(np.linalg.inv((self.X.T @ self.X)))
-        plt.imshow(self.covariance_matrix, extent=[0, 1, 0, 1])
-        plt.show()
-
-
-a = Model(100, 40, 5)
-a.estimate_least_squares()
+a = Model(30, 4, 5)
+a.calculate_error()
 print((a.a))
-print((a.a_approx))
+# print((a.a_approx))
 # a.plot_white_noise()
-a.plot_all()
-a.plot_imagesc()
-print(a.covariance_matrix)
+# a.plot_all()
+# a.plot_imagesc()
+# print(a.covariance_matrix)
 
